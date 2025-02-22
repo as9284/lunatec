051f623fa13f0b337d07e560276b7d849bfd350e
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-02-22 19:32:23 +0200

    Update min height to dvh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Home = () => {
   return (
     <>
-      <div className="w-full min-h-screen flex justify-center items-center p-4 pt-20">
+      <div className="w-full min-dvh-screen flex justify-center items-center p-4 pt-20">
         <h1 className="text-3xl font-sans">Home</h1>
       </div>
     </>
--- src/pages/Laptops.jsx
+++ src/pages/Laptops.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Laptops = () => {
   return (
     <>
-      <div className="w-full min-h-screen flex justify-center items-center p-4 pt-20">
+      <div className="w-full min-dvh-screen flex justify-center items-center p-4 pt-20">
         <h1 className="text-3xl font-sans">Laptops</h1>
       </div>
     </>
